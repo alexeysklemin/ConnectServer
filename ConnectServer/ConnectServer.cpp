5554594dd24cd414db74f0c712c716b65b7ed0a2
a_skl <a_skl@LAPTOP-ALEX>
2022-06-20 21:38:52 +0400

    processMessage

--- ConnectServer/ConnectServer.cpp
+++ ConnectServer/ConnectServer.cpp
@@ -1,14 +1,32 @@
 
 #include <iostream>
 #include <uwebsockets/App.h>
-//#include <nlohmann/json.hpp>
+#include <nlohmann/json.hpp>
 
 
 //Strucrure is joined to each connection..
+using json = nlohmann::json;
+
 struct UserData {
     int user_id;
     std::string name;
 };
+
+const std::string COMMAND = "COMMAND";
+const std::string PRIVATE_MSG = "PRIVATE_MSG";
+const std::string USER_ID_TO = "user_id_to";
+const std::string TEXT = "text";
+
+void processMessage(auto *ws, auto message) {
+    json data = json::parse(message);
+
+    switch (data[COMMAND]){
+    case PRIVATE_MSG:
+        break;
+    case PUBLIC_MSG:
+        break;
+    }
+}
 
 int main() {
     int latestUserId = 10;
@@ -21,16 +39,22 @@ int main() {
             }).ws<UserData>("/*", {
 
                 /* Just a few of the available handlers */
-                .open = [](auto* ws) {
+                .open = [&latestUserId](auto* ws) {
                     UserData* data = ws->getUserData();
                     data->user_id = latestUserId++;
+                    std::cout << "User connected ID " << data->user_id << std::endl;
 
                     /* MQTT syntax */
-                    ws->subscribe("sensors/+/house");
+                   // ws->subscribe("sensors/+/house");
                 },
                 .message = [](auto* ws, std::string_view message, uWS::OpCode opCode) {
-                    ws->send(message, opCode);
-                }
+                   // ws->send(message, opCode);
+                    processMessage(ws, message);
+                },
+                .close = [](auto* ws, int code, std::string_view message) {
+                    //
+}
+
 
                 }).listen(9001, [](auto* listenSocket) {
 
